37519d37c53ffa6bf685b8300044f7709d7b1be4
kamilbolibrzuch <kamil.bolibrzuch@gmail.com>
2022-06-26 21:51:33 +0200

    podstawowy szablon strony głównej, latestpostscomponent, wwyświetlanie ostatnio dodanych postów

---
+++ frontend/src/components/posts/LatestPostsComponent.vue
@@ -0,0 +1,205 @@
+<template>
+    <!--Posty-->
+    <div class="row" >
+        <div class="col-lg-3 col-md-6 mb-4 card-box" v-for="post in latestPosts" v-bind:key="post.id">
+          <router-link v-bind:to="post.get_absolute_url">
+            <MDBCard class="shadow-custom " style="border-radius: 10px;" >
+              <div class="bg-image" >
+              
+                <MDBCardImg id="zdjecie" class="img-fluid" v-bind:src="post.get_thumbnail"   />
+              </div>
+              <MDBCardBody class="card-body">
+                <MDBCardTitle class="card-title" style="text-decoration: none; color:black;" >{{ post.name }}</MDBCardTitle>
+                <MDBCardTitle class="card-title zdjecie-text1_pies" >{{ post.name }}</MDBCardTitle><br>
+                
+                <span style="color: white"><router-link v-bind:to="post.get_absolute_url" class="btn" id="btn_pies">szczegóły</router-link></span>
+              </MDBCardBody>
+            </MDBCard>
+          </router-link>
+        </div>
+     <!--Psy-->
+
+     <!-- Zobacz więcej postów -->
+        <div class="col-lg-3 col-md-6 mb-4 card-box" >
+          <MDBCard class="shadow-custom" style="border-radius: 10px;" id="background_dog">
+            <router-link to="/posts/1/order_by=from-newest" >
+              <div class="bg-image">
+                <div  id="zdj_wiecej" class="img-fluid">
+                  <div id="wiecej_psow"><b><br>Zobacz więcej<br>postów</b></div>
+                </div>
+              </div>
+              <MDBCardBody class="card-body">
+              </MDBCardBody>
+            </router-link>
+          </MDBCard>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImg, MDBBtn, mdbRipple } from "mdb-vue-ui-kit";
+
+
+export default {
+    name: 'LatestPostsComponent',
+    data(){
+    return{
+      latestPosts: [],
+    }
+  },
+  components: {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImg,
+    MDBBtn
+    
+  },
+  mounted() {
+    this.getLatestPosts(),
+    document.title = 'Zaadoptuj-zwierzaka'
+  },
+   methods: {
+ 
+  
+   async getLatestPosts() {
+    
+      await axios
+        .get('api/furniture_app/latest-posts/')
+        .then(response => {
+          this.latestPosts = response.data
+          console.table(this.latestPosts);
+          
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      //this.$store.commit('setIsLoading', false) 
+    }
+}
+}
+</script>
+
+<style>
+.card-box {
+  position: relative;
+  display: flex;
+	justify-content: space-around;
+  align-items: flex-end;
+	transition: 0.4s ease-out;
+	box-shadow: 0px 7px 10px rgba(black, 0.5);
+}
+.card-box span {
+  display: none;
+  position: absolute;
+  top: 45%;
+  transform: translateY(-50%);
+  left: 50%;
+  transform: translateX(-50%);
+}
+.card-box:hover span{
+  display: table-cell;  
+}
+
+.card-box:hover #zdjecie{
+  -webkit-filter: blur(5px); 
+}
+.card-box:hover {
+  transform: translateY(5px);
+}
+
+.bg-image{
+  object-fit: cover;
+}
+
+.shadow-custom{
+     box-shadow: 0 2px 20px 0 rgba(0, 0, 0, .25), 0 3px 8px 0px rgba(0, 0, 0, 0.13) !important;
+}
+.card-box:hover .zdjecie-text1_pies,.card-box:hover .zdjecie-text2 {
+  visibility: visible;
+  opacity: 1;
+}
+.zdjecie-text1_pies {
+  font-size: 40px;
+  position: absolute;
+  top: 10%;
+	left: 0;
+	display: block;
+	width: 100%;
+	height: 100%;
+	opacity: 0;
+  visibility: hidden;
+ 
+  text-shadow: white 0.1em 0.1em 0.2em;
+   -webkit-text-fill-color: rgb(49, 31, 71);
+  -webkit-text-stroke-color: rgb(255, 255, 255);
+  -webkit-text-stroke-width: 0.50px; 
+}
+
+.zdjecie-text2 {
+  font-size: 25px;
+  position: absolute;
+  top: 20%;
+	left: 0;
+	display: block;
+	width: 100%;
+	height: 100%;
+	opacity: 0;
+  visibility: hidden;
+  color: black;
+  text-shadow: white 0.1em 0.1em 0.2em
+}
+
+#btn_pies {
+  color: #ffff;
+  text-decoration: none;
+  background: #5b2c6f;
+  padding: 30px;
+  border-radius: 10px;
+  display: inline-block;
+  border: none;
+  transition: all 0.4s ease 0s;
+}
+#btn_pies:hover {
+  background: #2d57b1;
+  letter-spacing: 1px;
+  color: aliceblue;
+  -webkit-box-shadow: 0 5px 40px -10px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0 5px 40px -10px rgba(0, 0, 0, 0.57);
+  box-shadow: 5px 40px -10px rgba(0, 0, 0, 0.57);
+  transition: all 0.4s ease 0s;
+}
+
+#wiecej_psow {
+text-shadow: 2px 2px 4px #000000;
+  font-size: 50px;
+  color:rgb(88, 77, 149);
+  padding: 20px 0;
+  text-align: center;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+
+#zdj_wiecej {
+border-radius: 10px;
+  width: 350px;
+  height: 356px;
+}
+
+#zdjecie {
+          width: 350px;
+          height: 300px;
+}
+
+
+
+
+  
+ 
+        
+
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,19 +1,130 @@
 <template>
   <div>
     <navbar></navbar>
-    sadfdf
+    <!-- komponent nawigacji -->
+    <div class="home">
+      <div class="text-center" id="background_" style="height: 20em">
+        <h1 style=" color: white; padding-top: 25px; font-size: 55px; text-shadow: 2px 2px 4px #000000;">Nazwa firmy</h1>
+        <p style="color: white; padding-top: 175px; font-size: 25px; text-shadow: 2px 2px 4px #000000;">Jakiś napis</p>
+      </div>
+
+      <br />
+
+      <!--Głowny layout-->
+      <main class="my-5">
+        <div class="container">
+          <!--Psy-->
+          <section class="text-center">
+            <h4 class="mb-5">
+              <strong style="font-size: 30px">Ostatnio dodane posty</strong>
+            </h4>
+
+            <LatestPostsComponent></LatestPostsComponent>
+            </section>
+
+      
+        </div>
+      </main>
+      <!--Main layout-->
+
+      <!--Stopka-->
+      <footer class="bg-light text-lg-start">
+        <hr class="m-0" />
+
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+          © 2022 Copyright:
+          <a class="text-dark">Zaadoptujzwierzaka.pl</a>
+        </div>
+        <!-- Copyright -->
+      </footer>
+      <!--Footer-->
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/ui/Navbar.vue";
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import LatestPostsComponent from "@/components/posts/LatestPostsComponent";
+import { mdbRipple, MDBBtnGroup, MDBBtn } from "mdb-vue-ui-kit";
+import axios from "axios";
 export default {
-  name: 'HomeView',
+  name: "Home",
+  data() {
+    return {
+      dogs: 0,
+      adopted_dogs: 0,
+      cats: 0,
+      adopted_cats: 0,
+    };
+  },
   components: {
     Navbar,
-    HelloWorld
+    LatestPostsComponent,
+    MDBBtnGroup,
+    MDBBtn,
+  },
+
+  beforeMount() {
+    document.title = "Zaadoptuj-zwierzaka";
+
+  },
+  directives: {
+    mdbRipple,
+  },
+  methods: {
+   
+  },
+};
+</script>
+
+<style>
+#link {
+  color: white;
+  text-decoration: none;
+}
+
+
+#przycisk {
+  transition: all 0.5s;
+  cursor: pointer;
+  font-size: 28px;
+  font-family: "Molle", cursive;
+}
+
+#przycisk span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+#przycisk span:after {
+  content: "\00bb";
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+#przycisk:hover span {
+  padding-right: 25px;
+}
+#przycisk:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+#background_ {
+  background-color: rgb(13, 89, 89);
+  background-size: cover;
+}
+@media screen and (max-width: 500px) {
+  #background_ {
+    
+    background-size: cover;
   }
 }
-</script>
+
+</style>
